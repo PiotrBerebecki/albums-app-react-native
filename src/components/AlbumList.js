dd8aa6f754b8cd24df2569266e3fc273993c772b
PiotrBerebecki <petebere@gmail.com>
2016-10-22 09:46:44 +0100

    Fetch album data from the API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,8 +3,14 @@ import {
   View,
   Text,
 } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  componentDidMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
+  }
+  
   render() {
     return (
       <View>
