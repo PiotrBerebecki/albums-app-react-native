b709538e9d9931bff6a9101576f57b4048a613d1
PiotrBerebecki <petebere@gmail.com>
2016-10-24 12:45:27 +0100

    Display album art

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { 
+    thumbnail_image, 
+    title, 
+    artist, 
+    image } = album;
+    
+  const { 
+    headerThumbnailContainer,
+    headerThumbnail, 
+    headerTextContainer,
+    headerTextAlbum,
+    albumMainImage } = styles;
+    
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+    
+      <CardSection>
+      
+        <View style={headerThumbnailContainer}>
+          <Image
+            style={headerThumbnail} 
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        
+        <View style={headerTextContainer}>
+          <Text style={headerTextAlbum}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+        
+      </CardSection>
+        
+        
+      <CardSection>
+      
+        <Image
+          style={albumMainImage}
+          source={{ uri: image }}
+        />
+        
+      </CardSection>
+      
     </Card>
   );
+};
+
+const styles = {
+  headerThumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerThumbnail: {
+    height: 50,
+    width: 50
+  },
+  headerTextContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextAlbum: {
+    fontSize: 18
+  },
+  albumMainImage: {
+    height: 300,
+    width: null,
+    flex: 1
+  }
 };
 
 export default AlbumDetail;
